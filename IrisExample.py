5cf94773ba447e0f371b06136d7e38c1394b45f3
Shaaheen Sacoor <sacoorshaheen@gmail.com>
2017-08-23 12:00:11 +0200

    Project Initiation - Hons
    Added example ML program - iris
    Used as reference for other Machine Learning applications

---
+++ IrisExample.py
@@ -0,0 +1,49 @@
+
+# Load dataset
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = pandas.read_csv(url, names=names)
+
+print(dataset.shape)
+
+print(dataset.head(20))
+
+print(dataset.describe())
+
+array = dataset.values
+print("start")
+X = array[:, 0:4]
+Y = array[:, 4]
+#print(array[:, 4])
+validation_size = 0.20
+seed = 7
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size,
+                                                                                random_state=seed)
+
+seed = 7
+scoring = 'accuracy'
+
+models = []
+models.append(('LR', LogisticRegression()))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC()))
+# evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+    kfold = model_selection.KFold(n_splits=10, random_state=seed)
+    cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
+
+knn = KNeighborsClassifier()
+knn.fit(X_train, Y_train)
+predictions = knn.predict(X_validation)
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
